2e163aad439bbb6554ec25d51d9c51e368cb7173
Euxen <kelvinhezhang@live.com>
2024-08-19 17:55:17 -0400

    ending here for today for the navbar UI

--- my-app/app/components/navbar/Navbar.tsx
+++ my-app/app/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
+
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-20 shadow-sm">
@@ -9,6 +11,7 @@ const Navbar = () => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>

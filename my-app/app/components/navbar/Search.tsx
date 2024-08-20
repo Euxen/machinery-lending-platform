a2e7025f7dccf0bbf6ffdfc5544ca556f463430a
Euxen <kelvinhezhang@live.com>
2024-08-20 09:15:36 -0400

    git commit before starting

--- my-app/app/components/navbar/Search.tsx
+++ my-app/app/components/navbar/Search.tsx
@@ -29,7 +29,7 @@ const Search = () => {
           Any week
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">Add Equipment</div>
           <div className="p-2 text-white bg-rose-500 rounded-full">
             <BiSearch size={18} />
           </div>

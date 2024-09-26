e6c619f8064cb74868caf64b7c26c3737983fec2
Euxen <kelvinhezhang@live.com>
2024-09-26 16:23:46 -0400

    saving changes

--- my-app/app/components/CategoryBox.tsx
+++ my-app/app/components/CategoryBox.tsx
@@ -1,21 +1,54 @@
 "use client";
-
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FC, useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
+  label: string;
+  description: string;
   icon: IconType;
-  label: String;
-  selected: boolean;
+  selected?: boolean;
 }
 
-const CategoryBox: React.FC<CategoryBoxProps> = ({
-  icon: Icon,
+const CategoryBox: FC<CategoryBoxProps> = ({
   label,
+  description,
+  icon: Icon,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
   return (
     <div
+      onClick={handleClick}
       className={`
     flex
     flex-col
@@ -32,6 +65,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
     `}
     >
       <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
     </div>
   );
 };
